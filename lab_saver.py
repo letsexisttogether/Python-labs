c366dcc0d3bd36fc8d16cb35ea80f6e49bc52214
letsexisttogether <71719320+letsexisttogether@users.noreply.github.com>
2022-02-19 16:45:21 +0200

    Update lab_saver.py

--- lab_saver.py
+++ lab_saver.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import prettytable as pretty
 
 
 def MakeNewAx(figure, pos, name):
-    '''Функція для створення і налаштування нового графіку
+    """Функція для створення і налаштування нового графіку
     :return Об'єкт нового графіку
-    '''
+    """
     ax = figure.add_subplot(1, 2, pos)
     ax.set_title(name)
     ax.set_xlabel("Вісь X")
@@ -14,12 +15,12 @@ def MakeNewAx(figure, pos, name):
 
 
 def FloatValueEnter(msg):
-    '''Функція для зчитування float числа
+    """Функція для зчитування float числа
 
     msg - Повідомлення для вводу
 
     :return Число з комою
-    '''
+    """
     while True:
         floatVal = input(msg)
         # перевірка на можливість конвертації у число
@@ -31,17 +32,16 @@ def FloatValueEnter(msg):
             return float(floatVal)
 
 
-def PrintTable(x_start, x_end, step):
-    '''Функція для виводу таблиці у консоль
+def PrintTable():
+    """Функція для виводу таблиці у консоль """
+    table = pretty.PrettyTable(['Значення X', 'Значення x / cos(x)'])
+    for x in np.arange(x_start, x_end + step, step):
+        if x > x_end:
+            x = x_end
+        table.add_row(['{:.5f}'.format(x),
+                       '{:.5f}'.format(x / np.cos(x))])
+    print(table)
 
-    x_start - початкове значення x
-    x_end - кінцеве значення x \
-    step - шаг табуляції для таблиці
-    '''
-    array_x = np.linspace(x_start, x_end, int((x_end - x_start) / step))
-    print("X\tf(X)")
-    for x in array_x:
-        print("{:.5f}\t{:.5f}" .format(x, x / np.cos(x)))
 
 # Введення початкового, кінцевого x та кроку табуляції
 x_start = FloatValueEnter('Введіть початкову точку Х: ')
@@ -53,24 +53,25 @@ if step == 0:
     step = (x_end - x_start) / 10
 
 # Виведення таблиці значення x, f(x) на екран
-PrintTable(x_start, x_end, step)
+PrintTable()
 
-plt.style.use('seaborn-whitegrid')
 # Створення поля для графіків
+plt.style.use('seaborn-whitegrid')
 figure = plt.figure(figsize=(12, 5))
+
+# Створення головного графіку
 mainAx = MakeNewAx(figure, 1, 'Графік')
 mainAx.set_xlim(x_start, x_end)
 mainAx.set_ylim(-4 * np.abs(x_start), 4 * x_end)
 
-array_x = np.linspace(x_start, x_end, 10000)    # Список значень x на заданому проміжку
-array_y = np.linspace(x_start, x_end, 10000)    # Список значень функції x/cos(x)
+array_x = np.linspace(x_start, x_end, 10000)  # Список значень x на заданому проміжку
+array_y = np.linspace(x_start, x_end, 10000)  # Список значень функції x/cos(x)
 
-
-zero_points = []    # Список для зберігання точок розриву
+zero_points = []  # Список для зберігання точок розриву
 for i in range(len(array_x)):
     if np.abs(np.cos(array_x[i])) < 0.001:  # Якщо знайдено точку розриву
-        zero_points.append(array_x[i])      # Додаємо її у відповідний список
-        array_x[i] = np.nan                 # У списка X це значення = None
+        zero_points.append(array_x[i])  # Додаємо її у відповідний список
+        array_x[i] = np.nan  # У списка X це значення = None
     else:
         array_y[i] = array_x[i] / np.cos(array_x[i])
 
@@ -99,13 +100,13 @@ low = np.where((array_y[1:-1] > array_y[0:-2]) * \
 # Виведення графіку функції
 extraAx.plot(array_x, array_y, color='black')
 # Виведення точок мінінмуму та максиму
-extraAx.scatter(array_x[high], array_y[high], \
+extraAx.scatter(array_x[high], array_y[high],
                 marker='o', label='Точки локального максимуму')
-extraAx.scatter(array_x[low], array_y[low], \
+extraAx.scatter(array_x[low], array_y[low],
                 marker='o', label='Точки локального мінімуму')
 # Так як функція має всього одну точку перетину
 # з осями - виведення її без додаткових операцій
-extraAx.scatter(0, 0, marker='o', color='black', \
+extraAx.scatter(0, 0, marker='o', color='black',
                 label='Єдина точка перетину з Ox та Oy')
 
 extraAx.legend(loc='upper right')
