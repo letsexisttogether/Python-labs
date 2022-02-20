be768cf5f5bc485e24f4505c5d912fb991b2e0ed
letsexisttogether <71719320+letsexisttogether@users.noreply.github.com>
2022-02-20 13:42:33 +0200

    Update lab_saver.py

--- lab_saver.py
+++ lab_saver.py
@@ -4,8 +4,8 @@ import prettytable as pretty
 
 
 def MakeNewAx(figure, pos, name):
-    """Функція для створення і налаштування нового графіку
-    :return Об'єкт нового графіку
+    """Crates a graphic and presets it
+    :return Object of created graphic
     """
     ax = figure.add_subplot(1, 2, pos)
     ax.set_title(name)
@@ -15,15 +15,16 @@ def MakeNewAx(figure, pos, name):
 
 
 def FloatValueEnter(msg):
-    """Функція для зчитування float числа
+    """Enter a value
+    and check if the value entered is float
 
-    msg - Повідомлення для вводу
+    msg - a message of entering
 
-    :return Число з комою
+    :return float value
     """
     while True:
         floatVal = input(msg)
-        # перевірка на можливість конвертації у число
+        # check whether the entered value is float
         try:
             taker = float(floatVal)
         except ValueError:
@@ -33,7 +34,7 @@ def FloatValueEnter(msg):
 
 
 def PrintTable():
-    """Функція для виводу таблиці у консоль """
+    """The function prints the table of x f(x) into console"""
     table = pretty.PrettyTable(['Значення X', 'Значення x / cos(x)'])
     for x in np.arange(x_start, x_end + step, step):
         if x > x_end:
@@ -43,7 +44,7 @@ def PrintTable():
     print(table)
 
 
-# Введення початкового, кінцевого x та кроку табуляції
+# Entering start, end x and the tabulation step
 x_start = FloatValueEnter('Введіть початкову точку Х: ')
 x_end = FloatValueEnter('Введіть кінцеву точку X: ')
 if x_end < x_start:
@@ -52,60 +53,60 @@ step = np.abs(FloatValueEnter('Введіть крок табуляції: '))
 if step == 0:
     step = (x_end - x_start) / 10
 
-# Виведення таблиці значення x, f(x) на екран
+# Printing the x f(x) table
 PrintTable()
 
-# Створення поля для графіків
+# Creating a field to place graphics on
 plt.style.use('seaborn-whitegrid')
 figure = plt.figure(figsize=(12, 5))
 
-# Створення головного графіку
+# Main graphic creating
 mainAx = MakeNewAx(figure, 1, 'Графік')
 mainAx.set_xlim(x_start, x_end)
 mainAx.set_ylim(-4 * np.abs(x_start), 4 * x_end)
 
-array_x = np.linspace(x_start, x_end, 10000)  # Список значень x на заданому проміжку
-array_y = np.linspace(x_start, x_end, 10000)  # Список значень функції x/cos(x)
+array_x = np.linspace(x_start, x_end, 10000)  # List of x values [x_start, x_end]
+array_y = np.linspace(x_start, x_end, 10000)  # List of x/cos(x) values
 
-zero_points = []  # Список для зберігання точок розриву
+zero_points = []  # List of breakpoints
 for i in range(len(array_x)):
-    if np.abs(np.cos(array_x[i])) < 0.001:  # Якщо знайдено точку розриву
-        zero_points.append(array_x[i])  # Додаємо її у відповідний список
-        array_x[i] = np.nan  # У списка X це значення = None
+    if np.abs(np.cos(array_x[i])) < 0.001:  # If a breakpoint is found
+        zero_points.append(array_x[i])  # Add the breakpoint to the list
+        array_x[i] = np.nan
     else:
         array_y[i] = array_x[i] / np.cos(array_x[i])
 
-# Виведення графіку на полі figure
+# Printing the main graphic on the field (figure)
 mainAx.plot(array_x, array_y, color='black', label='y = x / cos (x)')
 mainAx.legend(loc='upper right')
 
-# Виведення асимптот на точках розриву
+# Printing asymptotes
 for x in zero_points:
     mainAx.axvline(x=x, linestyle='--', label='')
 
-# Створення додаткового графіку для особливих точок
+# Creating extra graphic for special points
 extraAx = MakeNewAx(figure, 2, 'Особливі точки')
 extraAx.set_xlim(x_start, x_end)
 extraAx.set_ylim(-4 * np.abs(x_start), 4 * x_end)
 
-# Пошук максимумів функції на заданому проміжку
-# за допомогою індексування
+# Finding max values of x / cos(x) on the interval
+# by indexing operation
 high = np.where((array_y[1:-1] < array_y[0:-2]) * \
                 (array_y[1:-1] < array_y[2:]))[0]
-# Пошук мінімумів функції на заданому проміжку
-# за допомогою індексування
+# Finding min values of x / cos(x) on the interval
+# by indexing operation
 low = np.where((array_y[1:-1] > array_y[0:-2]) * \
                (array_y[1:-1] > array_y[2:]))[0]
 
-# Виведення графіку функції
+# Printing x / cos(x) graphic
 extraAx.plot(array_x, array_y, color='black')
-# Виведення точок мінінмуму та максиму
+# Printing min and max values on the extra graphic
 extraAx.scatter(array_x[high], array_y[high],
                 marker='o', label='Точки локального максимуму')
 extraAx.scatter(array_x[low], array_y[low],
                 marker='o', label='Точки локального мінімуму')
-# Так як функція має всього одну точку перетину
-# з осями - виведення її без додаткових операцій
+# Since the function only has one point of crossing
+# Ox and Oy - print the point without extra operations
 extraAx.scatter(0, 0, marker='o', color='black',
                 label='Єдина точка перетину з Ox та Oy')
 
